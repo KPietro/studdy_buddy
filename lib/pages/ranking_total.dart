99942023c7c0c5d47c49f0254f6d45513055be3c
brayan-henrique <brayan.henrique200808@gmail.com>
2026-04-17 13:45:54 -0300

    tentando enterar o firebase,nas ranking_total.dart e ramking_semanl.dart

--- lib/pages/ranking_total.dart
+++ lib/pages/ranking_total.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart'; // Importação do pacote da curva
-import 'ranking_semanal.dart'; // Importe o outro arquivo aqui
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'ranking_semanal.dart';
 
 class RankingTotal extends StatefulWidget {
   const RankingTotal({super.key});
@@ -12,25 +13,12 @@ class RankingTotal extends StatefulWidget {
 class _RankingTotalState extends State<RankingTotal> {
   bool isTemaEscuro = true;
 
-  // Paleta de Cores
   final Color bgEscuro = const Color(0xFF2B0505);
   final Color baseEscura = const Color(0xFF4A0000);
   final Color bgClaro = const Color(0xFFEAFaf1);
   final Color baseClara = const Color(0xFF4CAF50);
   final Color verdemeiescuro = const Color.fromRGBO(25, 170, 45, 1);
   final Color verdeNeon = const Color.fromARGB(255, 55, 255, 20);
-
-  // Dados do Total (Pontuações maiores)
-  final List<Map<String, dynamic>> jogadores = List.generate(
-    15,
-    (index) => {
-      "posicao": index + 1,
-      "nome": index == 1
-          ? "Pietro Lagos"
-          : (index == 2 ? "Tim" : "Brayan Henrique"),
-      "pontos": 300000 - (index * 1000),
-    },
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -39,14 +27,51 @@ class _RankingTotalState extends State<RankingTotal> {
     return Scaffold(
       backgroundColor: isDark ? bgEscuro : bgClaro,
       body: SafeArea(
-        child: Column(
-          children: [
-            _buildHeader(isDark),
-            const SizedBox(height: 20),
-            _buildPodio(isDark),
-            const SizedBox(height: 30),
-            Expanded(child: _buildLista(isDark)),
-          ],
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('grupos')
+              .doc('G1')
+              .collection('membros')
+              .orderBy('pontosTotais', descending: true) // ORDENA PELO TOTAL
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Text(
+                  'Nenhum jogador encontrado.',
+                  style: TextStyle(color: isDark ? Colors.white : Colors.black),
+                ),
+              );
+            }
+
+            final List<Map<String, dynamic>> jogadores = [];
+            final docs = snapshot.data!.docs;
+
+            for (int i = 0; i < docs.length; i++) {
+              jogadores.add({
+                "posicao": i + 1,
+                "nome": docs[i]['nome'] ?? "Sem Nome",
+                "pontos": docs[i]['pontosTotais'] ?? 0, // PUXA O TOTAL
+                "atividades":
+                    docs[i]['atividadesMaioresTotais'] ?? 0, // PUXA O TOTAL
+                "fotoPerfil": docs[i]['fotoPerfil'],
+              });
+            }
+
+            return Column(
+              children: [
+                _buildHeader(isDark),
+                const SizedBox(height: 20),
+                _buildPodio(isDark, jogadores),
+                const SizedBox(height: 30),
+                Expanded(child: _buildLista(isDark, jogadores)),
+              ],
+            );
+          },
         ),
       ),
       bottomNavigationBar: _buildBottomBar(isDark, context),
@@ -62,7 +87,6 @@ class _RankingTotalState extends State<RankingTotal> {
     );
   }
 
-  // --- CABEÇALHO ---
   Widget _buildHeader(bool isDark) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
@@ -76,7 +100,6 @@ class _RankingTotalState extends State<RankingTotal> {
               onPressed: () {},
             ),
           ),
-
           Expanded(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -105,37 +128,40 @@ class _RankingTotalState extends State<RankingTotal> {
     );
   }
 
-  // --- PÓDIO 3D ---
-  Widget _buildPodio(bool isDark) {
+  Widget _buildPodio(bool isDark, List<Map<String, dynamic>> jogadores) {
     return SizedBox(
       height: 250,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          _buildPilar(2, 142, isDark),
+          if (jogadores.length > 1) _buildPilar(jogadores[1], 142, isDark),
           const SizedBox(width: 15),
-          _buildPilar(1, 160, isDark),
+          if (jogadores.isNotEmpty) _buildPilar(jogadores[0], 160, isDark),
           const SizedBox(width: 15),
-          _buildPilar(3, 124, isDark),
+          if (jogadores.length > 2) _buildPilar(jogadores[2], 124, isDark),
         ],
       ),
     );
   }
 
-  Widget _buildPilar(int posicao, double altura, bool isDark) {
+  Widget _buildPilar(Map<String, dynamic> j, double altura, bool isDark) {
     const double larguraFrente = 50.0;
     const double depth = 15.0;
     const double angle = 0.5;
     final double dy = depth * angle;
-    int alturaquero = altura.toInt();
+    int alturaquero = altura.toInt(); // Sua lógica mantida
+
     final colorFront = isDark ? Colors.greenAccent : Colors.greenAccent[400]!;
     final colorSide = isDark ? Colors.green[800]! : Colors.green[700]!;
     final colorTop = isDark ? Colors.green[900]! : Colors.green[800]!;
 
+    bool temFoto =
+        j["fotoPerfil"] != null && j["fotoPerfil"].toString().isNotEmpty;
+
     return TweenAnimationBuilder<double>(
       tween: Tween<double>(begin: 0, end: altura),
-      duration: Duration(milliseconds: 10 * alturaquero),
+      duration: Duration(milliseconds: 10 * alturaquero), // Sua lógica mantida
       curve: Curves.easeOutCubic,
       builder: (context, alturaAnimada, child) {
         return Column(
@@ -144,10 +170,12 @@ class _RankingTotalState extends State<RankingTotal> {
             CircleAvatar(
               radius: 22,
               backgroundColor: isDark ? Colors.grey[800] : Colors.grey[400],
-              child: const Icon(Icons.person, color: Colors.white),
+              backgroundImage: temFoto ? NetworkImage(j["fotoPerfil"]) : null,
+              child: !temFoto
+                  ? const Icon(Icons.person, color: Colors.white)
+                  : null,
             ),
             const SizedBox(height: 10),
-
             CustomPaint(
               size: Size(larguraFrente + depth, alturaAnimada + dy),
               painter: PillarPainter(
@@ -163,19 +191,21 @@ class _RankingTotalState extends State<RankingTotal> {
     );
   }
 
-  // --- LISTA DE JOGADORES ---
-  Widget _buildLista(bool isDark) {
+  Widget _buildLista(bool isDark, List<Map<String, dynamic>> jogadores) {
     return ListView.builder(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       itemCount: jogadores.length,
       itemBuilder: (context, index) {
         final j = jogadores[index];
+        bool temFoto =
+            j["fotoPerfil"] != null && j["fotoPerfil"].toString().isNotEmpty;
+
         return Container(
           margin: const EdgeInsets.only(bottom: 7),
           padding: const EdgeInsets.all(4),
           decoration: BoxDecoration(
             color: isDark ? Colors.black : Colors.white,
-            border: Border.all(),
+            border: Border.all(color: Colors.white24),
             borderRadius: BorderRadius.circular(3),
           ),
           child: Row(
@@ -197,10 +227,13 @@ class _RankingTotalState extends State<RankingTotal> {
                         ),
                       ),
               ),
-              const CircleAvatar(
+              CircleAvatar(
                 radius: 12,
                 backgroundColor: Colors.grey,
-                child: Icon(Icons.person, size: 15),
+                backgroundImage: temFoto ? NetworkImage(j["fotoPerfil"]) : null,
+                child: !temFoto
+                    ? const Icon(Icons.person, size: 15, color: Colors.white)
+                    : null,
               ),
               const SizedBox(width: 10),
               Expanded(
@@ -211,7 +244,7 @@ class _RankingTotalState extends State<RankingTotal> {
               ),
               Container(
                 padding: const EdgeInsets.symmetric(
-                  horizontal: 22,
+                  horizontal: 10,
                   vertical: 3,
                 ),
                 decoration: BoxDecoration(
@@ -221,7 +254,7 @@ class _RankingTotalState extends State<RankingTotal> {
                 child: Text(
                   'PTS   ${j["pontos"]}',
                   style: const TextStyle(
-                    color: Color.fromARGB(249, 255, 255, 255),
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontSize: 12,
                   ),
@@ -235,9 +268,9 @@ class _RankingTotalState extends State<RankingTotal> {
                   borderRadius: BorderRadius.circular(3),
                 ),
                 child: Text(
-                  'QTT:10',
+                  'QTT:${j["atividades"]}',
                   style: const TextStyle(
-                    color: Color.fromRGBO(2555, 255, 255, 1),
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontSize: 12,
                   ),
@@ -250,27 +283,19 @@ class _RankingTotalState extends State<RankingTotal> {
     );
   }
 
-  // --- BARRA INFERIOR CURVADA ---
   Widget _buildBottomBar(bool isDark, BuildContext context) {
     return CurvedNavigationBar(
-      // A cor do fundo tem que ser a mesma do Scaffold
       backgroundColor: isDark ? bgEscuro : bgClaro,
-      // A cor da barra e do botão flutuante
       color: isDark ? baseEscura : baseClara,
       buttonBackgroundColor: isDark ? baseEscura : baseClara,
       height: 60,
       animationDuration: const Duration(milliseconds: 300),
-
-      // AQUI ESTÁ O SEGREDO DO TOTAL: O index inicial é 1 (o segundo botão)
       index: 1,
-
       items: const <Widget>[
         Icon(Icons.emoji_events, color: Colors.deepOrange, size: 30),
         Icon(Icons.workspace_premium, color: Colors.yellow, size: 30),
       ],
-
       onTap: (index) {
-        // Se clicar no troféu laranja (index 0), volta para o Semanal
         if (index == 0) {
           Future.delayed(const Duration(milliseconds: 300), () {
             Navigator.pushReplacement(
@@ -288,9 +313,6 @@ class _RankingTotalState extends State<RankingTotal> {
   }
 }
 
-// ==========================================
-// CLASSE DO PILAR 3D
-// ==========================================
 class PillarPainter extends CustomPainter {
   final double altura;
   final Color colorFront;
@@ -306,6 +328,8 @@ class PillarPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    if (altura <= 0.1) return;
+
     const double W = 50.0;
     const double D = 15.0;
     const double angle = 0.5;
@@ -334,14 +358,12 @@ class PillarPainter extends CustomPainter {
       ..lineTo(W, Dy)
       ..lineTo(0, Dy)
       ..close();
-
     final pathSide = Path()
       ..moveTo(W, base)
       ..lineTo(W + D, base - Dy)
       ..lineTo(W + D, 0)
       ..lineTo(W, Dy)
       ..close();
-
     final pathTop = Path()
       ..moveTo(0, Dy)
       ..lineTo(W, Dy)
